4ec40dcf077e962d79494003d462a51e56857a2b
LucianoBonilauri <luchobonilauri@gmail.com>
2026-05-18 14:31:15 -0300

    Hola

---
+++ Assets/Scripts/InteractiveArea.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveArea : MonoBehaviour
+{
+    UIManager uiManagerScript;
+    public bool isInteracting = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        uiManagerScript = GameObject.FindObjectOfType<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other){
+        Interactable interactableScript = other.GetComponent<Interactable>();
+        if(interactableScript != null)
+        {
+            uiManagerScript.ShowCartelPresione();
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+        uiManagerScript.HideCartelPresione();
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject goCartelPresione;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goCartelPresione = GameObject.FindGameObjectWithTag("CartelPresione");
+        goCartelPresione.SetActive(false);
+    }
+
+    public void ShowCartelPresione(){
+        goCartelPresione.SetActive(true);
+    }
+
+    public void HideCartelPresione(){
+        goCartelPresione.SetActive(false);
+    }
+
+}
